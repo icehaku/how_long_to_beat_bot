a88c1f695129bc231504768eff3165b15ac7a559
Elano Garcez <elanogarcez@gmail.com>
2017-08-09 12:04:05 -0300

    beta version 1.0

--- app/controllers/concerns/inline_bot.rb
+++ app/controllers/concerns/inline_bot.rb
@@ -10,64 +10,68 @@ module InlineBot
     	query = params["inline_query"]["query"]
     end
 
-    url = URI.encode("https://howlongtobeat.com/search_main.php?page=1")
-    party_response = HTTParty.post(url,
-      body: {
-        queryString:"pokemon",
-        t:"games",
-        sorthead:"popular",
-        sortd:"Normal Order",
-        length_type:"main",
-      })
+    begin
+      url = URI.encode("https://howlongtobeat.com/search_main.php?page=1")
+      party_response = HTTParty.post(url,
+        body: {
+          queryString: query,
+          t: "games",
+          sorthead: "popular",
+          sortd: "Normal Order",
+          length_type: "main",
+        })
 
-    result = Nokogiri::HTML(party_response)
-    result = result.css("li.back_white")
+      result = Nokogiri::HTML(party_response)
+      result = result.css("li.back_white")
+    rescue
+      result = nil
+    end
 
-    #https://howlongtobeat.com/gameimages/
     games = []
+    if result.present?
+      result.each do |game|
+        game_ready = Hash.new()
+        begin
+          game_ready["name"] = game.css("div.search_list_image").css("a").attribute("title").value
+          game_ready["image"] = game.css("div.search_list_image").css("a").css("img").attribute("src").value
+          game_ready["url"] = game.css("div.search_list_image").css("a").attribute("href").value
 
-    result.each do |game|
-      game_ready = Hash.new()
-      #raise game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text.inspect
+          game_ready["categoria"] = ""
+          if game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].present?
+            game_ready["categoria"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].text
+          end
 
-      game_ready["name"] = game.css("div.search_list_image").css("a").attribute("title").value
-      game_ready["image"] = game.css("div.search_list_image").css("a").css("img").attribute("src").value
+          game_ready["categoria_tempo"] = ""
+          if game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].present?
+            game_ready["categoria_tempo"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text
+          end
 
-      if game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].present?
-        game_ready["categoria"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].text
+          if game.css("div.search_list_details_block").css("div.search_list_tidbit").present?
+            games << game_ready
+          end
+        rescue
+        end
       end
 
-      if game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].present?
-        game_ready["categoria_tempo"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text
+      token = '386847090:AAGmVhpUjbkmZKvqE8Wasx8PKRfBHh0domk' #How Long to Beat Bot
+      bot = Telegram::Bot::Client.new(token)
+
+      results = games.map.with_index do |game, index|
+        {
+          id: index.to_s,
+          type: "article",
+          title: game['name'],
+          description: "",
+          thumb_url: "https://howlongtobeat.com/#{game['image']}",
+          input_message_content: {
+            parse_mode: "HTML",
+            message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b> \n #{game_ready['categoria']}: #{game_ready['categoria_tempo']} \n https://howlongtobeat.com/#{game['url']}",
+          },
+        }
       end
 
-      #search_list_tidbit_short
-      #search_list_tidbit_long
-      if game.css("div.search_list_details_block").css("div.search_list_tidbit").present?
-        games << game_ready
-      end
+      bot.answer_inline_query inline_query_id: inline_query_id ,results: results
     end
-
-    token = '386847090:AAGmVhpUjbkmZKvqE8Wasx8PKRfBHh0domk' #How Long to Beat Bot
-    bot = Telegram::Bot::Client.new(token)
-
-    results = games.map.with_index do |game, index|
-      {
-        id: index.to_s,
-        type: "article",
-        title: game['name'],
-        description: game['name'],
-        thumb_url: "https://howlongtobeat.com/gameimages/#{game['image']}",
-        input_message_content: {
-          parse_mode: "HTML",
-          message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b> /n https://howlongtobeat.com/gameimages/#{game['image']}",
-        },
-      }
-    end
-
-    #raise results.inspect
-
-    bot.answer_inline_query inline_query_id: inline_query_id ,results: results
 	end
 
 end
--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -7,16 +7,16 @@ class TelegramController < ApplicationController
 	include Bot
 	include InlineBot
 
-  skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
 
-  def callback
-    #observer(params)
-    inline(params)
+	def callback
+	    #observer(params)
+	    inline(params)
 
-    render body: nil, head: :ok
-  end
+	    render body: nil, head: :ok
+	end
 
-  def hi
+	def hi
 		#require 'no_proxy_fix'
 
 		url = URI.encode("https://howlongtobeat.com/search_main.php?page=1")
@@ -32,15 +32,13 @@ class TelegramController < ApplicationController
 		result = Nokogiri::HTML(party_response)
 		result = result.css("li.back_white")
 
-		#https://howlongtobeat.com/gameimages/
 		games = []
-
 		result.each do |game|
 			game_ready = Hash.new()
-			#raise game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text.inspect
 
 			game_ready["name"] = game.css("div.search_list_image").css("a").attribute("title").value
 			game_ready["image"] = game.css("div.search_list_image").css("a").css("img").attribute("src").value
+			game_ready["url"] = game.css("div.search_list_image").css("a").attribute("href").value
 
 			if game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].present?
 				game_ready["categoria"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].text
@@ -50,11 +48,9 @@ class TelegramController < ApplicationController
 				game_ready["categoria_tempo"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text
 			end
 
-			#search_list_tidbit_short
-			#search_list_tidbit_long
 			if game.css("div.search_list_details_block").css("div.search_list_tidbit").present?
 				games << game_ready
 			end
 		end
-  end
+	end
 end
