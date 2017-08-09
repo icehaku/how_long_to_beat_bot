cb2212ee51c1929e2957059c5637e7272399022f
Elano Garcez <elanogarcez@gmail.com>
2017-08-09 10:58:46 -0300

    alpha version

--- app/controllers/concerns/inline_bot.rb
+++ app/controllers/concerns/inline_bot.rb
@@ -10,41 +10,43 @@ module InlineBot
     	query = params["inline_query"]["query"]
     end
 
-		url = URI.encode("http://www.metacritic.com/search/all/#{query}/results")
-		user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
+    url = URI.encode("https://howlongtobeat.com/search_main.php?page=1")
+    party_response = HTTParty.post(url,
+      body: {
+        queryString:"pokemon",
+        t:"games",
+        sorthead:"popular",
+        sortd:"Normal Order",
+        length_type:"main",
+      })
 
-		begin
-			results = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
-    rescue OpenURI::HTTPError => ex
-    	results = nil
-    end
+    result = Nokogiri::HTML(party_response)
+    result = result.css("li.back_white")
 
+    #https://howlongtobeat.com/gameimages/
     games = []
-    if results.present?
-    	results = results.css("li.result")
 
-	    results.each do |result|
-	    	if result.css("div.result_type").css("strong").text == "Game"
-					game = Hash.new()
-					game["name"] = result.css("a").text
-					game["release"] = result.css("li.stat.release_date").css("span.data").text
-					game["platform"] = result.css("span.platform").text
-					game["metascore"] = result.css("span.metascore_w").text
-					game["description"] = result.css("p.deck.basic_stat").text
-					game["url"] = result.css("a")[0]['href']
-					game["url"] = "http://www.metacritic.com#{game["url"]}"
-          if game["metascore"].present?
-            game["image"] = set_metascore_image(game["metascore"])
-            game["metascore"] = "#{game['metascore']}"
-          else
-            game["image"] = "http://www.headslinger.com/feed_img/1000565.jpg"
-          end
-					#game["image_userscore"] = get_metacritic_image_userscore_by_url(game["url"])
+    result.each do |game|
+      game_ready = Hash.new()
+      #raise game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text.inspect
 
-					games << game
-				end
-	    end
-	  end
+      game_ready["name"] = game.css("div.search_list_image").css("a").attribute("title").value
+      game_ready["image"] = game.css("div.search_list_image").css("a").css("img").attribute("src").value
+
+      if game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].present?
+        game_ready["categoria"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[0].text
+      end
+
+      if game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].present?
+        game_ready["categoria_tempo"] = game.css("div.search_list_details_block").css("div.search_list_tidbit")[1].text
+      end
+
+      #search_list_tidbit_short
+      #search_list_tidbit_long
+      if game.css("div.search_list_details_block").css("div.search_list_tidbit").present?
+        games << game_ready
+      end
+    end
 
     token = '386847090:AAGmVhpUjbkmZKvqE8Wasx8PKRfBHh0domk' #How Long to Beat Bot
     bot = Telegram::Bot::Client.new(token)
@@ -52,13 +54,13 @@ module InlineBot
     results = games.map.with_index do |game, index|
       {
         type: "article",
-        title: "#{game['name']}(#{game['platform']}) MS:#{game['metascore']}",
+        title: game['name'],
         id: index.to_s,
-        description: game["description"],
-        thumb_url: game["image"],
+        description: game['name'],
+        thumb_url: 'https://howlongtobeat.com/gameimages/#{game["image"]}',
         input_message_content: {
           parse_mode: "HTML",
-          message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b>\n<b>Console</b>: #{game['platform']}\n<b>Metascore</b>: #{game['metascore']}\n<b>Release</b>: #{game['release']}\n#{game['description']}\n#{game['url']}",
+          message_text: "\xF0\x9F\x91\xBE <b>#{game['name']}</b>",
         },
       }
     end
@@ -66,30 +68,4 @@ module InlineBot
     bot.answer_inline_query inline_query_id: inline_query_id ,results: results
 	end
 
-
-  def get_metacritic_image_userscore_by_url(url)
-    begin
-      user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.854.0 Safari/535.2"
-      url = "http://www.metacritic.com#{url}"
-      page = Nokogiri::HTML(open(url, 'User-Agent' => user_agent), nil, "UTF-8")
-      image = page.css("img.product_image.large_image")[0].values[1]
-      userscore = page.css("div.product_scores").css("div.metascore_w.user.large.game")[0].text
-      [image, userscore]
-    rescue
-      ["", ""]
-    end
-  end
-
-
-  def set_metascore_image(score)
-    grade = score.to_i
-
-    if grade > 75
-      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=33cc33&txtclr=ffffff"
-    elsif grade > 45
-      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=fed731&txtclr=ffffff"
-    else
-      "https://placeholdit.imgix.net/~text?txtsize=33&txt=#{score}&150&w=75&h=75&bg=cc0000&txtclr=ffffff"
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 	root to: 'telegram#hi'
 
-	#post '/webhooks/telegram_XY9wMhL0M9Y0B2DBt0H22HFd01969KaB', to: 'telegram#callback'
+	post '/webhooks/telegram_HLTBMhL0M9Y0B2DBt0H22HFd01969ICE', to: 'telegram#callback'
 	#get '/webhooks/telegram_XY9wMhL0M9Y0B2DBt0H22HFd01969KaB', to: 'telegram#callback'
 	#get '/metacritic_inline', to: 'telegram#metacritic_inline'
 	get '/hi', to: 'telegram#hi'
 end
+
+#https://how-long-beat-bot.herokuapp.com/webhooks/telegram_HLTBMhL0M9Y0B2DBt0H22HFd01969ICE
+#https://api.telegram.org/bot386847090:AAGmVhpUjbkmZKvqE8Wasx8PKRfBHh0domk/setWebhook
